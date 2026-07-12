61972179e26b28fd5780729c8d681b5a6204b20a
hrishabhsoni4764 <47682516+hrishabhsoni4764@users.noreply.github.com>
2026-07-12 23:25:06 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int buyMin=prices[0];
+        for(int price: prices)
+        {
+            buyMin = std::min(buyMin,price);
+            profit = std::max(profit,price-buyMin);
+        }
+        return profit;
+    }
+};
